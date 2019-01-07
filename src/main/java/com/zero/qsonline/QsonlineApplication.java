62f1fe90a344d69255616d8ee996770108ea59d5
Zero <2721002772@qq.com>
2019-01-07 14:39:52 +0800

    First push

---
+++ src/main/java/com/zero/qsonline/QsonlineApplication.java
@@ -0,0 +1,14 @@
+package com.zero.qsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QsonlineApplication.class, args);
+    }
+
+}
+
